6ee36e94ab11b9a7ab9a18166785c5a5aca43b25
CITLALI_AILEEN_MISCLES_RUVALCABA <a.22308051281264@cbtis128.edu.mx>
2025-05-09 03:45:46 +0000

    subida

---
+++ lib/pagina_dos.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PantallaDos extends StatefulWidget {
+  const PantallaDos({Key? key}) : super(key: key);
+
+  @override
+  State<PantallaDos> createState() => _PantallaDosState();
+}
+
+class _PantallaDosState extends State<PantallaDos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pantalla Dos'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Mostrar About Dialog'),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => const AboutDialog(
+                applicationIcon: FlutterLogo(),
+                applicationLegalese: 'Legalese',
+                applicationName: 'Flutter App',
+                applicationVersion: 'versión 1.0.0',
+                children: [
+                  Text('Este es un texto creado por Flutter Mapp'),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
